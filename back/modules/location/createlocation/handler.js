84f1e60dff0be5ea339d6785afacf5e9ca913e54
kut_arika <kut.arika.1114@gmail.com>
2015-12-20 21:17:24 +0900

    サーバー側のプロジェクトを作成する。 #53
    一覧取得と作成、とりあえず固定レスポンスを返却するように変更

--- back/modules/location/createlocation/handler.js
+++ back/modules/location/createlocation/handler.js
@@ -15,8 +15,21 @@ var lib = require('../lib');
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
+  var response = {
+    "_id": "zzzzzzzzz",
+    "type": "ヒヤリハット",
+    "address": "葵区追手町９−６",
+    "location": {
+      "lat": 34.9738584,
+      "lon": 138.3851473
+    },
+    "title": "静岡県庁",
+    "description": "詳細情報画面に表示する説明文",
+    "preset": false
+  };
+  return context.done(null, response);
 
-  lib.respond(event, function(error, response) {
-    return context.done(error, response);
-  });
+  // lib.respond(event, function(error, response) {
+  //   return context.done(error, response);
+  // });
 };
--- back/modules/location/readlocations/handler.js
+++ back/modules/location/readlocations/handler.js
@@ -15,8 +15,37 @@ var lib = require('../lib');
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
+  var response = {
+    "results": [
+      {
+        "_id": "xxxxxxxx",
+        "type": "幼稚園",
+        "address": "葵区追手町５−１",
+        "location": {
+          "lat": 34.9738584,
+          "lon": 138.3851473
+        },
+        "title": "静岡市役所",
+        "description": "詳細情報画面に表示する説明文だよ",
+        "preset": true
+      },
+      {
+        "_id": "yyyyyyyyy",
+        "type": "ヒヤリハット",
+        "address": "駿河区南八幡町１０−４０",
+        "location": {
+          "lat": 34.9738584,
+          "lon": 138.3851473
+        },
+        "title": "駿河区役所",
+        "description": "詳細情報画面に表示する説明文です",
+        "preset": true
+      }
+    ]
+  };
+  return context.done(null, response);
 
-  lib.respond(event, function(error, response) {
-    return context.done(error, response);
-  });
+  // lib.respond(event, function(error, response) {
+  //   return context.done(error, response);
+  // });
 };
